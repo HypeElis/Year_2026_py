1c3df0b78c5f55d02b99677a425bdbc82095c550
HypeElis <ar2727397@gmail.com>
2026-07-01 11:43:26 +0530

    Add files via upload

---
+++ Guess the Number.py
@@ -0,0 +1,22 @@
+#Game where you find the right number
+print("Welcom to My Gussing Game")
+print()
+print("Guess the correct number")
+num = int(input("Choose a number between 1 to 100\nchoose = "))
+print()
+
+
+
+if num <= 10:
+    print("Too far")
+elif num <= 50:
+    print("Close")
+elif num <= 60:
+    print("Close")  
+elif num <=100:
+    print("Too far")      
+
+
+
+
+    
